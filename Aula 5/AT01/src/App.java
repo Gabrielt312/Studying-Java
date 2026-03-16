d88944524b1bd8dfd71e70468b3bee8f588e7390
Gabriel <132002025+Gabrielt312@users.noreply.github.com>
2026-03-16 20:02:12 -0300

    Aula 5 arrays at01

---
+++ Aula 5/AT01/src/App.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in); //Leitor do teclado
+        int valores[] = new int[10];
+        
+        //Ler valores
+        for(int i=0; i<10; i++){
+            System.out.println(i +" Digite um número");
+            valores[i]= sc.nextInt();
+        }
+
+        //Exibir valores
+
+        for(int j =0; j<10; j++){
+            System.out.print("|" + valores[j]);
+        }
+
+        System.out.println("Digite o valor minimo: ");
+        int minimo = sc.nextInt();
+
+        System.out.println("Digite o valor maximo: ");
+        int maximo = sc.nextInt();
+
+        System.out.println("Valores dentro do intervalo");
+
+        for(int x = 0; x < 10; x++){
+            if (valores[x] >= minimo && valores[x] <= maximo) {
+               System.out.print(" - "+valores[x]); 
+            }
+        }
+        sc.close();
+    }
+}
